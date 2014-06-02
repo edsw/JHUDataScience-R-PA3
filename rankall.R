9cac55195648f91216de2f0b7aa553c51d556fa9
Ed Swindelles <ed@swindelles.com>
2014-06-01 23:42:11 -0400

    Simplified data structures...data.frame -> matrix.

--- rankall.R
+++ rankall.R
@@ -7,31 +7,26 @@ rankall <- function(outcome, num = "best") {
 	csv    <- csv[order(csv$State, csv$Hospital.Name),]
 	states <- unique(csv$State)	
 	column <- ifelse(outcome == "heart attack", 11, ifelse(outcome == "heart failure", 17, 23))
-	output <- data.frame(hospital = character(), state = character(), stringsAsFactors = FALSE)
+	output <- NULL
 
 	for (i in 1:length(states)) {
 		sub  <- subset(csv, State == states[i])
 		vals <- suppressWarnings(as.numeric(sub[, column]))
 
 		if (num == "best") {
-			output <- rbind(output, data.frame(
-				hospital = sub[which.min(vals),2],
-				state = states[i], stringsAsFactors=FALSE))
+			output <- rbind(output, matrix(c(sub[which.min(vals),2], states[i]), ncol = 2))
 		}
 		else if (num == "worst") {
-			output <- rbind(output, data.frame(
-				hospital = sub[which.max(vals),2],
-				state = states[i], stringsAsFactors=FALSE))
+			output <- rbind(output, matrix(c(sub[which.max(vals),2], states[i]), ncol = 2))
 		}
 		else if (is.numeric(num)) {
-			output <- rbind(output, data.frame(
-				hospital = sub[order(vals)[num],2][1],
-				state = states[i], stringsAsFactors=FALSE))
+			output <- rbind(output, matrix(c(sub[order(vals)[num],2][1], states[i]), ncol = 2))
 		}
 		else {
 			stop("invalid num")
 		}
 	}
 
-	output
+	colnames(output) <- c("hospital", "state")
+	as.data.frame(output)
 }
