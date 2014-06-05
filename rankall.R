9c70fb2be85fcafde120600d0f679a9c735d8052
Ed Swindelles <ed@swindelles.com>
2014-06-05 15:34:02 -0400

    Added functions for testing aggregate() and data.table()

--- rankall.R
+++ rankall.R
@@ -1,3 +1,5 @@
+library("data.table")
+
 rankall <- function(outcome, num = "best") {
 	if (!outcome %in% c("heart attack", "heart failure", "pneumonia")) {
 		stop("invalid outcome")
@@ -29,4 +31,48 @@ rankall <- function(outcome, num = "best") {
 
 	colnames(output) <- c("hospital", "state")
 	as.data.frame(output)
+}
+
+rankall2 <- function(outcome, num = "best") {
+	if (!outcome %in% c("heart attack", "heart failure", "pneumonia")) {
+		stop("invalid outcome")
+	}
+
+	csv    <- read.csv("outcome-of-care-measures.csv", colClasses = "character")
+	column <- ifelse(outcome == "heart attack", 11, ifelse(outcome == "heart failure", 17, 23))
+	colnames(csv)[column] <- "Outcome"
+	
+	output <- aggregate(Outcome ~ State, csv, function(x) {
+		x <- as.numeric(x)
+		y <- NULL
+		if (num == "best") {
+			y <- min(x, na.rm = TRUE)
+		} else if (num == "worst") {
+			y <- max(x, na.rm = TRUE)
+		}
+		else if (is.numeric(num)) {
+			y <- sort(x)[num][1]
+		}
+		else {
+			stop("invalid num")
+		}
+		as.character(y)
+	})
+
+	output
+}
+
+rankall3 <- function(outcome, num = "best") {
+	if (!outcome %in% c("heart attack", "heart failure", "pneumonia")) {
+		stop("invalid outcome")
+	}
+
+	csv    <- read.csv("outcome-of-care-measures.csv", colClasses = "character")
+	column <- ifelse(outcome == "heart attack", 11, ifelse(outcome == "heart failure", 17, 23))
+	colnames(csv)[column] <- "Outcome"
+
+	DT <- data.table(csv)
+	setkey(DT, "State")
+	a <- DT[, list(Outcome = min(Outcome, na.rm = TRUE)), by=key(DT)]
+	a
 }
